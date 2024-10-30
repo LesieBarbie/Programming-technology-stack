b53fcd261c2f5faddd47f91edc505dc3061b0556
LesieBarbie <olesia.pushkina@student.karazin.ua>
2024-10-30 13:50:33 +0200

    Added unary minus handling, checking for redundant operators, and division by 0

--- Prakt2/RPN.rb
+++ Prakt2/RPN.rb
@@ -1,54 +1,109 @@
 def priority(operator)
-  if operator == '+' || operator == '-'
-    1
-  elsif operator == '*' || operator == '/'
-    2
-  elsif operator == '^'
-    3
-  else
-    0
+  case operator
+  when '+', '-' then 1
+  when '*', '/' then 2
+  when '^' then 3
+  else 0
   end
 end
 
 def convert_rpn(expression)
   result = []
   stack = []
-
-  expression_space = expression.gsub(/([\+\-\*\/\^\(\)])/,' \1 ')
+  expression_space = expression.gsub(/([+\-*\/^()])/,' \1 ')
   elements = expression_space.split
+  last_token = nil
 
-  elements.each do |element|
-    if element =~ /\w+/
-      result.push(element)
+  elements.each_with_index do |element, index|
+    if element =~ /^-?\d+(\.\d+)?$/ || (element == '-' && (last_token.nil? || %w[+ - * / (].include?(last_token)))
+      # Перевірка на унарний мінус як частину числа
+      if element == '-' && (last_token.nil? || %w[+ - * / (].include?(last_token))
+        number = "-#{elements[index + 1]}"
+        result.push(number)
+        elements[index + 1] = ''  # Уникнемо повторної обробки числа
+      else
+        result.push(element)
+      end
     elsif element == '('
       stack.push(element)
     elsif element == ')'
       while stack.last != '('
+        raise "Невірне використання дужок" if stack.empty?
         result.push(stack.pop)
       end
       stack.pop
     else
+      # Перевірка на зайві оператори підряд
+      if element =~ /[+\-*\/^]/ && %w[+ - * / ^ (].include?(last_token)
+        raise " зайвий оператор '#{element}'"
+      end
+
+      # Перевірка на зайвий оператор в кінці виразу
+      if index == elements.size - 1 && element =~ /[+\-*\/^]/
+        raise " вираз закінчується оператором '#{element}'"
+      end
+
+      # Обробка операторів за пріоритетом
       while !stack.empty? && priority(stack.last) >= priority(element)
         result.push(stack.pop)
       end
       stack.push(element)
     end
+    last_token = element
   end
 
+  # Витягуємо всі оператори зі стеку
   while !stack.empty?
+    raise " зайва дужка" if stack.last == '(' || stack.last == ')'
     result.push(stack.pop)
   end
 
   result.join(' ')
 end
 
-# Основний цикл для вводу виразів
+def evaluate_rpn(rpn_expression)
+  stack = []
+
+  rpn_expression.split.each do |token|
+    if token =~ /^-?\d+(\.\d+)?$/
+      stack.push(token.to_f)
+    else
+      b = stack.pop
+      a = stack.pop
+      case token
+      when '+'
+        stack.push(a + b)
+      when '-'
+        stack.push(a - b)
+      when '*'
+        stack.push(a * b)
+      when '/'
+        raise " ділення на 0" if b == 0
+        stack.push(a / b)
+      when '^'
+        stack.push(a**b)
+      else
+        raise "Невідомий оператор: #{token}"
+      end
+    end
+  end
+
+  raise " зайві операнди" if stack.size != 1
+  stack.pop
+end
+
 loop do
   puts "Введіть математичний вираз (або 'exit' для виходу):"
   input = gets.chomp
 
-  break if input.downcase == 'exit'  # Вихід з програми
+  break if input.downcase == 'exit'
 
-  rpn = convert_rpn(input)
-  puts "Вираз у RPN: #{rpn}"
+  begin
+    rpn = convert_rpn(input)
+    puts "Вираз у RPN: #{rpn}"
+    result = evaluate_rpn(rpn)
+    puts "Результат обчислення: #{result}"
+  rescue => e
+    puts "Помилка: #{e.message}"
+  end
 end
