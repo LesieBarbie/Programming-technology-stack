2d80847c9eebd085f1659867c7260a564328b60d
LesieBarbie <olesia.pushkina@student.karazin.ua>
2024-11-21 11:24:36 +0200

    Initial commit

--- Prakt4/app.rb
+++ Prakt4/app.rb
@@ -7,26 +7,30 @@ API_KEY = 'e0cfeb8ae5e3f14a48cc858f312808d7'
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 def get_weather(city)
-  url = URI("#{BASE_URL}?q=#{URI.encode_www_form_component(city)}&appid=#{API_KEY}&units=metric")
+  url = URI("#{BASE_URL}?q=#{URI.encode_www_form_component(city)}&appid=#{API_KEY}&units=metric&lang=ua")
   response = Net::HTTP.get_response(url)
 
-  return JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
-
-  { 'error' => 'Failed to fetch weather data', 'code' => response.code }
+  if response.is_a?(Net::HTTPSuccess)
+    JSON.parse(response.body)
+  else
+    { 'error' => response.message, 'code' => response.code }
+  end
 end
 
 def extract_weather_data(data)
-  return nil if data.key?('error')
+  required_keys = %w[name main wind weather]
+  return nil unless required_keys.all? { |key| data.key?(key) }
 
   {
     city: data['name'],
     temperature: data.dig('main', 'temp'),
     humidity: data.dig('main', 'humidity'),
     wind_speed: data.dig('wind', 'speed'),
-    weather_description: data.dig('weather', 0, 'description')
+    weather_description: data.dig('weather', 0, 'description') || 'Невідомо'
   }
 end
 
+# Збереження даних у CSV
 def save_to_csv(data, filename = 'weather_output.csv')
   CSV.open(filename, 'a+', headers: data.keys, write_headers: !File.exist?(filename)) do |csv|
     csv << data.values
@@ -41,8 +45,9 @@ def display_weather(data)
   puts "Опис погоди: #{data[:weather_description]}"
 end
 
+# Основна програма
 def main
-  puts 'Введіть назву міста для отримання погоди:'
+  puts 'Введіть назву міста англійскою мовою для отримання погоди:'
   city = gets.chomp
 
   weather_data = get_weather(city)
